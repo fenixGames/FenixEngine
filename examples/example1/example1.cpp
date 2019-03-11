ee15ece85280ea38d4d1868fa50e3900f9d5c15d
Lara Fernandez Cueto <r.fernandez-cueto@bally-wulff.de>
2019-03-11 18:07:49 +0100

    Adding a example to the project

---
+++ examples/example1/example1.cpp
@@ -0,0 +1,22 @@
+/** Creating a basic window using FenixEngine
+ *
+ * The goal is to create a very basic window using the FenixEngine
+ * with a scene and waits for the user to close the window.
+ */
+
+#include <FenixEngine/FenixEngine.h>
+
+#define WIN_WIDTH 640
+#define WIN_HEIGHT 480
+
+int
+main(int argc, char **argv)
+{
+	Window win(WIN_WIDTH, WIN_HEIGHT, "Example1");
+	Scene scene;
+	EventController controller;
+
+	scene.setRenderer(win.getRenderer());
+	scene.evController = &controller;
+	win.start();
+}
