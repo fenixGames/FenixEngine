30e9d0f0f49c6b6d56f9b2e7dfc8920800cc1c47
lara <larafercue@gmail.com>
2019-03-24 10:11:29 +0100

    Creating a model for game components

---
+++ include/game_component.h
@@ -0,0 +1,15 @@
+/** Describes components of a game object.
+ * This is a generic class that has to be extended.
+ */
+
+#ifndef GAME_COMPONENT_H
+#define GAME_COMPONENT_H
+
+#include <list>
+#include <typeinfo>
+
+class GameComponent {
+
+};
+
+#endif // !GAME_COMPONENT_H
